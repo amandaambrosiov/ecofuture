b50388ad9ffccba2255a2fd3635db0bdf8c3f12c
Amanda <amandaambrosio295@gmail.com>
2024-05-13 10:39:51 -0300

    subindo conteudo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,14 @@
-window.addEventListener('scroll', function() {
-    var header = document.getElementById('header');
-    if (window.scrollY > 0) {
-      header.classList.add('fixed');
-    } else {
-      header.classList.remove('fixed');
-    }
-  });
-  
+window.addEventListener('scroll', function () {
+  var header = document.getElementById('header');
+  if (window.scrollY > 0) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+});
+
+// Espera 3 segundos antes de mostrar o conteúdo
+setTimeout(function() {
+  document.querySelector('.loading').style.display = 'none';
+  document.querySelector('.body').style.display = 'block';
+}, 1000);
